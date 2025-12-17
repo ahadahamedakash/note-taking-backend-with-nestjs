f4c64630d09aed5982c323a02c264698aefd4315
ahadahamedakash <a.ahad2232@gmail.com>
2025-12-17 12:20:56 +0600

    feat: Implement password hashing and create jwt token for registering a user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,35 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
 import { RegisterDto } from './dto/register.dto';
 import { UserService } from 'src/user/user.service';
+import { ConflictException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
   async register(registerDto: RegisterDto) {
     const user = await this.userService.getUserByEmail(registerDto.email);
     if (user) {
       throw new ConflictException('Email already taken');
     }
-    // 1/47/33
-    return user;
+    const saltRounds = 10;
+
+    const hashedPass = await bcrypt.hash(registerDto.password, saltRounds);
+
+    const newUser = await this.userService.createUser({
+      ...registerDto,
+      password: hashedPass,
+    });
+
+    const payload = { sub: newUser.id, email: newUser.email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
