9872ac5ced884373d1bd5960abdf9c419825233a
ahadahamedakash <a.ahad2232@gmail.com>
2025-12-17 22:12:10 +0600

    feat: Add Swagger documentation and integrate Prisma service in Note module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,17 @@ async function bootstrap() {
     }),
   });
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Notes apis documentation')
+    .setDescription('The is the docuementation for notes API')
+    .setVersion('1.0')
+    .addTag('Notes')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
